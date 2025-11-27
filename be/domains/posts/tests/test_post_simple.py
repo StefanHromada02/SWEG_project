a419da95efef4f9b92d1b267baa05899be957f8d
stefanhromada <stefan.hromada@objectbay.com>
2025-11-27 16:49:15 +0100

    fix pipeline

--- be/domains/posts/tests/test_post_simple.py
+++ be/domains/posts/tests/test_post_simple.py
@@ -1,27 +1,28 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from domains.users.models import User
 from domains.posts.models import Post
 
 
 class PostSimpleTest(TestCase):
-    def test_create_post(self):
-        # Arrange: create a simple user
-        user = User.objects.create(
+    def setUp(self):
+        self.user = User.objects.create(
             name="Alice",
             email="alice@technikum-wien.at",
             study_program="Software Engineering",
+            interests=[],
+            created_at=timezone.now(),
         )
 
-        # Act: create a post for that user
+    def test_create_post(self):
         post = Post.objects.create(
-            user=user,
+            user=self.user,
             title="Hello World",
             text="This is my first post!",
             image="https://example.com/image.jpg",
         )
 
-        # Assert: basic invariants
         self.assertIsNotNone(post.id)
-        self.assertEqual(post.user, user)
+        self.assertEqual(post.user, self.user)
         self.assertEqual(str(post), "Hello World by Alice")
