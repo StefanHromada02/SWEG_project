753180133814a3e39b2f87e96cdb5e2a4bcd4448
stefanhromada <stefan.hromada@objectbay.com>
2025-11-27 15:28:48 +0100

    fix github action try 2

--- be/config/settings.py
+++ be/config/settings.py
@@ -94,20 +94,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-
-        # Werte aus der .env
-        'NAME': 'social_media_db',      # POSTGRES_DB
-        'USER': 'postgres',             # POSTGRES_USER
-        'PASSWORD': 'postgres',         # POSTGRES_PASSWORD
-
-        # Hostname aus docker-compose.yml
-        'HOST': 'db',                   # POSTGRES_HOST Service Name
-        'PORT': '5432',
+if os.getenv('DJANGO_DB', '').lower() == 'sqlite' or os.getenv('CI', '').lower() == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'social_media_db'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+            'HOST': os.getenv('DATABASE_HOST', os.getenv('POSTGRES_HOST', 'db')),
+            'PORT': os.getenv('DATABASE_PORT', os.getenv('POSTGRES_PORT', '5432')),
+        }
+    }
 
 # --- Zusätzliche Anpassungen für MinIO (Medien-Dateien) ---
 
