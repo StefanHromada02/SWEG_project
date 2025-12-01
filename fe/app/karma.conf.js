e5a15601a52fc32bf868d4bf2f8ec6049d8729c0
stefanhromada <stefan.hromada@objectbay.com>
2025-12-01 23:38:15 +0100

    fix git frontend tests try 17

--- fe/app/karma.conf.js
+++ fe/app/karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
 
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
