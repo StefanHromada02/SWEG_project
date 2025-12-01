2945e430539f32162aa2f879ecbbac84c4a6399f
stefanhromada <stefan.hromada@objectbay.com>
2025-12-01 23:32:16 +0100

    fix git frontend tests try 15

--- fe/app/karma.conf.js
+++ fe/app/karma.conf.js
@@ -31,9 +31,7 @@ module.exports = function (config) {
       customLaunchers: {
         ChromeHeadlessCI: {
           base: 'ChromeHeadless',
-          flags: [
-            '--no-sandbox'
-          ]
+          flags: ['--no-sandbox', '--disable-gpu']
         }
       },
     },
