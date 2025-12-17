775ec3dada320ead54c775f576d7157eb32349f7
Felix Hüfner <felix-huefner@gmx.de>
2025-12-17 20:09:56 +0100

    fix(tests): fix tests

--- be/domains/posts/tests/test_models.py
+++ be/domains/posts/tests/test_models.py
@@ -1,62 +1,54 @@
-"""
-from django.test import TestCase
-from django.contrib.auth.models import User
+import pytest
 from domains.posts.models import Post
-from django.utils import timezone
+from domains.users.models import User
 import time
 
 
-class PostModelTests(TestCase):
+@pytest.mark.django_db
+class TestPostModel:
+    """Tests for Post model."""
 
-    @classmethod
-    def setUpTestData(cls):
-        # Setup data that will be used by all test methods
-        cls.user = User.objects.create_user(username='testuser', password='password123')
-
-        # Create posts with controlled timing for sorting tests
-        cls.post_older = Post.objects.create(
-            user=cls.user,
-            title="Older Post",
-            text="This is an older post.",
-            image="http://example.com/older.jpg"
+    def test_post_creation(self):
+        """Test creating a post."""
+        user = User.objects.create(
+            name="Test User",
+            email="test@technikum-wien.at",
+            study_program="Software Engineering"
         )
-        # Ensure the next post is measurably newer
-        time.sleep(0.01)
-        cls.post_newer = Post.objects.create(
-            user=cls.user,
-            title="Newer Post",
-            text="This is a newer post.",
-            image="http://example.com/newer.jpg"
+        post = Post.objects.create(
+            user=user,
+            title="Test Post",
+            text="This is a test post.",
+            image="posts/test.jpg"
         )
-
-    def test_post_str_representation(self):
-        #Testet die __str__ Methode des Post-Modells.#
-        expected_str = f"{self.post_older.title} by {self.user.username}"
-        self.assertEqual(str(self.post_older), expected_str)
+        
+        assert post.id is not None
+        assert post.title == "Test Post"
+        assert post.user == user
+        assert str(post) == "Test Post by Test User"
 
     def test_manager_newest_first(self):
-        #Testet, ob der PostManager die Posts korrekt sortiert.#
-        # Ruft die QuerySet-Methode über den Manager auf
+        """Test that PostManager returns posts sorted by newest first."""
+        user = User.objects.create(
+            name="Test User",
+            email="test@technikum-wien.at",
+            study_program="Software Engineering"
+        )
+        
+        post_older = Post.objects.create(
+            user=user,
+            title="Older Post",
+            text="This is an older post.",
+            image=""
+        )
+        time.sleep(0.01)
+        post_newer = Post.objects.create(
+            user=user,
+            title="Newer Post",
+            text="This is a newer post.",
+            image=""
+        )
+        
         posts = Post.objects.newest_first()
-
-        self.assertEqual(posts.first(), self.post_newer, "Der neueste Post sollte an erster Stelle stehen.")
-        self.assertEqual(posts.last(), self.post_older, "Der älteste Post sollte an letzter Stelle stehen.")
-
-    def test_manager_with_author_prefetching(self):
-        #Testet, ob 'with_author' N+1 Abfragen verhindert.#
-
-        # We expect 2 queries:
-        # 1. Query for all posts
-        # 2. Query for all related users (prefetched by select_related)
-        # Wenn 'with_author' nicht funktioniert, wären es 1 (Posts) + N (Users) = 3 Queries.
-
-        with self.assertNumQueries(1):
-            # Hole alle Posts UND deren Autoren
-            posts = list(Post.objects.with_author())
-
-            # Greife auf die Autoren zu (löst die prefetched data aus)
-            for post in posts:
-                # This access would cause N+1 queries if 'with_author' failed
-                _ = post.user.username
-
-"""
+        assert posts.first() == post_newer
+        assert posts.last() == post_older
