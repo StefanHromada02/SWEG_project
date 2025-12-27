c6624321373d3b64fd03bff8ebb53a3d7d83ba3e
Felix Hüfner <felix-huefner@gmx.de>
2025-12-27 15:58:24 +0100

    feat(ai comments): added a ai comment functionality

--- fe/app/src/app/services/api.service.ts
+++ fe/app/src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { backendUrl } from '../../environments';
+import { backendUrl, environment } from '../../environments';
 import { PostModel } from '../models/post.model';
 import { CommentModel } from '../models/comment.model';
 
@@ -10,6 +10,7 @@ import { CommentModel } from '../models/comment.model';
 })
 export class ApiService {
   private readonly baseUrl: string = backendUrl.apiUrl;
+  private readonly aiServiceUrl: string = environment.aiServiceUrl;
 
   constructor(private readonly http: HttpClient) {}
 
@@ -41,5 +42,13 @@ export class ApiService {
   // Create a new comment
   createComment(comment: { post: number; text: string }): Observable<CommentModel> {
     return this.http.post<CommentModel>(`${this.baseUrl}/comments/`, comment);
+  }
+
+  // Generate AI sarcastic comment
+  generateAiComment(postTitle: string, postText: string): Observable<any> {
+    return this.http.post(`${this.aiServiceUrl}/generate-comment`, {
+      title: postTitle,
+      text: postText
+    });
   }
 }
